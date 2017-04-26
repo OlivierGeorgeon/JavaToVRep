408203386820dad1560dc10ed09888bb11fdf2b4
Olivier <olivier.georgeon@gmail.com>
2017-04-26 12:04:33 +0200

    Existence500PoppyTorso works with RoboticiaFist

--- JavaToVRep/src/existence/Existence500PoppyTorso.java
+++ JavaToVRep/src/existence/Existence500PoppyTorso.java
@@ -1,10 +1,10 @@
 package existence;
 
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import coppelia.CharWA;
 import coupling.interaction.Interaction030;
 import coupling.interaction.Interaction040;
 
@@ -12,9 +12,77 @@ import coupling.interaction.Interaction040;
 // Open V-REP
 // poppy-services --vrep --http poppy-torso
 
-public class Existence500PoppyTorso extends Existence050 {
+public class Existence500PoppyTorso extends Existence500 {
 
 	@Override
+	public Interaction040 enactPrimitiveIntearction(Interaction030 intendedPrimitiveInteraction){
+		
+		String experimentLabel = LABEL_E1;
+		String retultLabel = LABEL_R1;
+		if (intendedPrimitiveInteraction.getLabel().contains(LABEL_E2))
+			experimentLabel = LABEL_E2;
+		
+		// Execute the primitive experiment
+		try {
+			URL url = new URL("http://127.0.0.1:8080/primitive/" + experimentLabel + "/start.json");	
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	        connection.setInstanceFollowRedirects(false); 
+	        connection.setRequestMethod("GET"); 
+	        connection.setRequestProperty("Content-Type", "application/json"); 
+	        //connection.setDoOutput(true); 
+	        
+	        int responseCode = connection.getResponseCode(); 
+	        //System.out.println("Response Code : " + responseCode);
+			Thread.sleep(3000);
+	        
+	        //connection.disconnect(); 		
+	    } catch(Exception e) { 
+	        throw new RuntimeException(e); 
+	    } 
+
+		// read the result
+		try {
+			URL url = new URL("http://127.0.0.1:8080/primitive/" + experimentLabel + "/property/result");	
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	        connection.setInstanceFollowRedirects(false); 
+	        connection.setRequestMethod("GET"); 
+	        connection.setRequestProperty("Content-Type", "application/json"); 
+	        //connection.setDoOutput(true); 
+	        
+	        int responseCode = connection.getResponseCode(); 
+	        //System.out.println("Response Code : " + responseCode);
+	        
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String inputLine = in.readLine();
+			/*
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			System.out.println(response.toString());
+			*/
+			in.close();		
+			String loadString = inputLine.substring(14, 16);
+			System.out.println(loadString);
+			int presentLoad = Integer.valueOf(loadString);
+			
+			if (presentLoad > 0) 	
+				retultLabel = LABEL_R2;
+	        
+	        //connection.disconnect(); 		
+	    } catch(Exception e) { 
+	        throw new RuntimeException(e); 
+	    } 
+		
+		Interaction040 enactedInteraction = null;
+		enactedInteraction = this.addOrGetPrimitiveInteraction(experimentLabel + retultLabel, 0);
+
+    	return enactedInteraction;
+	}	
+
+	/* Method POST
 	public Interaction040 enactPrimitiveIntearction(Interaction030 intendedPrimitiveInteraction){
 		
 		String elbow = "r";
@@ -24,13 +92,21 @@ public class Existence500PoppyTorso extends Existence050 {
 		try {
 			URL url = new URL("http://127.0.0.1:8080/motor/" + elbow + "_elbow_y/register/goal_position/value.json");	
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	        connection.setDoOutput(true); 
 	        connection.setInstanceFollowRedirects(false); 
 	        connection.setRequestMethod("POST"); 
-	        connection.addRequestProperty("d", "0");
 	        connection.setRequestProperty("Content-Type", "application/json"); 
-	        connection.getResponseCode(); 
-	        connection.disconnect(); 		
+	        
+	        connection.setDoOutput(true); 
+	        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+	        wr.writeBytes("100");
+	        wr.flush();
+	        wr.close();
+	        
+	        int responseCode = connection.getResponseCode(); 
+	        
+	        System.out.println("Response Code : " + responseCode);
+	        
+	        //connection.disconnect(); 		
 	    } catch(Exception e) { 
 	        throw new RuntimeException(e); 
 	    } 
@@ -40,6 +116,6 @@ public class Existence500PoppyTorso extends Existence050 {
 		enactedInteraction = this.addOrGetPrimitiveInteraction(LABEL_E1 +LABEL_R1, 0);
 
     	return enactedInteraction;
-	}	
+	}	*/
 
 }
--- JavaToVRep/src/main/Main.java
+++ JavaToVRep/src/main/Main.java
@@ -4,6 +4,7 @@ import existence.Existence100;
 import existence.Existence310;
 import existence.Existence400;
 import existence.Existence500;
+import existence.Existence500PoppyTorso;
 
 public class Main
 {
@@ -12,9 +13,9 @@ public class Main
     	
 		//Existence existence = new Existence100();
 		//Existence existence = new Existence310();
-		Existence existence = new Existence500();
+		Existence existence = new Existence500PoppyTorso();
 		
-		for(int i = 0 ; i < 300 ; i++){			
+		for(int i = 0 ; i < 100 ; i++){			
 			String stepTrace = existence.step();
 			System.out.println(i + ": " + stepTrace);
 		}
